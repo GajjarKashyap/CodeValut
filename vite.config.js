e546508298a26fbb2e62484ad9441aadef578ef2
Kashyap Gajjar <gajjarkashyap@codevault.dev>
2026-06-21 17:04:23 +0530

    config: revert custom domain configurations and switch back to subdirectory routing /CodeValut/

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // GitHub Pages repo subdirectory
+  base: '/CodeValut/', // GitHub Pages repo subdirectory
 })

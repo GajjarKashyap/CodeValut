b54626a26055f3d9788d449883282f12bdc4ce8d
Kashyap Gajjar <gajjarkashyap@codevault.dev>
2026-06-21 16:35:37 +0530

    config: setup custom domain labcodevalut.qd.je with CNAME and root base paths

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CodeValut/', // GitHub Pages repo subdirectory
+  base: '/', // GitHub Pages repo subdirectory
 })
